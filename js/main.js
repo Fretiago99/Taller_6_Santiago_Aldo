c932c85625fbd0a37eb2eb3192a0ebd49549977a
Aldo Esteban Vera Zúñiga <aestebanveraz@gmail.com>
2023-11-22 21:11:30 -0300

    1, 2 & 3 and half

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-const titulo = document.head.children[2];
+// const titulo = document.head.children[2];
 
-console.log(titulo.innerHTML);
+// console.log(titulo.innerHTML);
+
+/* 1: Vincular un archivo "index.html" con uno de JS "js/main.js". En el title y h1 principal del archivo HTML incorporar el texto "Ejercicio". 
+Con JS leer directamente el contenido del elemento <title> e imprimirlo en la consola. */
+
+// Acceder al title de html leerlo y mostrarlo por consola
+console.log(document.title);
+
+// const aldo = document.getElementById("aldo").innerText;
+// const esteban = document.getElementById("esteban").innerText;
+// const vera = document.getElementById("vera").innerText;
+// const zúñiga = document.getElementById("zúñiga").innerText;
+
+// const santiago = document.getElementById("santiago").innerText;
+// const romero = document.getElementById("romero").innerText;
+
+/* \n */
+// console.log(
+//   `Integrante 1: ${aldo} ${esteban} ${vera} ${zúñiga}
+// Integrante 2: ${santiago} ${romero}`
+// );
+
+const dl1 = document.getElementById("list1").children;
+// console.log(dl1[1].innerText);
+console.log(dl1);
+
+const dl2 = document.getElementById("list2").children;
+console.log(dl2);
+
+function capture(dl1, dl2) {
+  dl1 = dl1[1].innerText;
+  dl2 = dl2[1].innerText;
+
+  return `Integrante 1: ${dl1} \nIntegrante 2: ${dl2}`;
+}
+
+console.log(capture(dl1, dl2));
