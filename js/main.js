79ec89e548ed576a0dc505c51e5519badfaa63a1
Aldo Esteban Vera Zúñiga <aestebanveraz@gmail.com>
2023-11-26 14:57:21 -0300

    deleting

--- js/main.js
+++ js/main.js
@@ -8,39 +8,14 @@ Con JS leer directamente el contenido del elemento <title> e imprimirlo en la co
 // Acceder al title de html leerlo y mostrarlo por consola
 console.log(document.title);
 
-// const aldo = document.getElementById("aldo").innerText;
-// const esteban = document.getElementById("esteban").innerText;
-// const vera = document.getElementById("vera").innerText;
-// const zúñiga = document.getElementById("zúñiga").innerText;
-
-// const santiago = document.getElementById("santiago").innerText;
-// const romero = document.getElementById("romero").innerText;
-
 /* \n */
-// console.log(
-//   `Integrante 1: ${aldo} ${esteban} ${vera} ${zúñiga}
-// Integrante 2: ${santiago} ${romero}`
-// );
-
-// const dl1 = document.getElementById("list1").children;
-// console.log(dl1[1].innerText);
-// console.log(dl1);
-
-// const dl2 = document.getElementById("list2").children;
-// console.log(dl2);
-
-// function capture(dl1, dl2) {
-//   dl1 = dl1[1].innerText;
-//   dl2 = dl2[1].innerText;
-
-//   return `Integrante 1: ${dl1} \nIntegrante 2: ${dl2}`;
-// }
-
-// console.log(capture(dl1, dl2));
 
 /* función para crear el nombre completo*/
 function createFullName(htmlCollection) {
   /* recibe como parametro el listado html q aparece en consola */
+  /* como es una concatenacion vamos instanciar una variable de tipo let */
+  let fullName = "";
+  /* le asignamos un valor de un string vacio */
 }
 
 const listOne = document.getElementById("list1").children;
@@ -48,3 +23,7 @@ const listOne = document.getElementById("list1").children;
 
 console.log(listOne);
 /* Muest */
+
+/* Ejemplo priemr integrante no tiene segundo nombre, y el segundo integrante no tiene segundo apellido
+entonces vamos a evaluar si viene ese dato, si viene le agregamos el espacio, y si no viene no se lo agregamos, para que quede todo automatizado
+ */
