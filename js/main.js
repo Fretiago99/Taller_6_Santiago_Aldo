fa8b36d9583988653885bbd8102b994b026a3634
Aldo Esteban Vera Zúñiga <aestebanveraz@gmail.com>
2023-11-27 15:45:44 -0300

    update

--- js/main.js
+++ js/main.js
@@ -14,16 +14,84 @@ console.log(document.title);
 function createFullName(htmlCollection) {
   /* recibe como parametro el listado html q aparece en consola */
   /* como es una concatenacion vamos instanciar una variable de tipo let */
+
+  /* le asignamos un valor de un string vacio, que va ser mi estado inicial, vamos a ir agregando nombres a este string */
   let fullName = "";
-  /* le asignamos un valor de un string vacio */
+
+  /* Vamos a hacer un for para pasar por las distintas etiquetas */
+  /* HTMLCollection va a ser la lista q aparecen en la consola el arrelgo, se van a separar en distintas variables y las voy a pasar siempre
+  por parametro  */
+  for (let index = 0; index < htmlCollection.length; index++) {
+    /* index comienza en 0, esto se va a correr siempre y cuando que index sea menor al largo del array, pero en este caso el array 
+    se llama htmlCollection q en este caso va a dar 8, sea va a ir de 0 a 7 y cada vuelta vamos a sumarle 1 al index */
+    const element = htmlCollection[index];
+    /* vamos a acceder al elemento de cada vuelta del htmlCollection */
+    if (element.tagName === "DD" && element.innerText) {
+      /* vamos a evaluar el elemento cuando el tagname o etiqueta sea DD */
+      /* si innertext es un espacio vacio osea es falso y si viene falso podemos decirle q solamente concatene cuando innertext tiene algo
+      osea cuandoe es true y viene con nombre */
+      /* solamente va a concatenar cuando sea DD */
+      fullName = fullName.concat(`${element.innerText} `);
+      /* en la primer vuelta se le agrego a fullname el nombre aldo, en la segunda vuelta se le agrego un string vacio por lo tanto
+      no suma nada a la variable, despues se agrego Vera y luego Zúñiga, el string vacio no influye, el strign vacio q se agrega no tiene ingerencia 
+      hace el concat pero no lo agrega*/
+      /* ahora se le da un espacio para que no quede todo el nombre pegado */
+      /* una de las formas de hacer la concatenacion es: */
+      // console.log(element.innerText);
+      // if (element.innerText) {
+      /* hay algo en element.innertext? si hay algo */
+      /* vamos a fullname q ya tiene concatenado el fullname de la linea 34 y le agregamos el espacio*/
+      //   fullName = fullName.concat(" ");
+      // }
+    }
+  }
+  return `"${fullName}"`;
 }
 
-const listOne = document.getElementById("list1").children;
+const fullList = document.getElementById("divList").children;
+// const listOne = document.getElementById("list1").children;
 /* capturo en una variable el listado 1 de la lista descriptiva con el integrante 1 */
 
-console.log(listOne);
-/* Muest */
+/* Funcion  para mostrar listado*/
+function showList(list) {
+  /* le pasamos por parametro la lista */
+  let text = "";
+  /* text va a ser el string total q tiene el nro de integrante y nombre de interante */
+  for (let i = 0; i < list.length; i++) {
+    const elementList = list[i];
+    /* logear elementList */
+    console.log(elementList);
+    if (elementList.getElementById === "H2") {
+      /* si es igual a h2 vamos a concatenar */
+      text = text.concat(`${elementList.innerText}: `);
+    }
+    if (elementList.tagName === "DL") {
+      text = text.concat(createFullName(elementList.children) + "\n");
+      /* aca concateno lo q me devuelve el return de la funcion createFullName, yo le paso los children de este DIV */
+      /* le enviamos por parametro el elemento de la lista .children */
+    }
+  }
+  return text;
+}
+/* llamar a la funcion para mostrarla */
+console.log(showList(fullList));
 
 /* Ejemplo priemr integrante no tiene segundo nombre, y el segundo integrante no tiene segundo apellido
 entonces vamos a evaluar si viene ese dato, si viene le agregamos el espacio, y si no viene no se lo agregamos, para que quede todo automatizado
  */
+
+/* al final llamamos a la funcion y le mandamos el listado pro parametro osea listOne */
+// console.log(createFullName(listOne));
+/* HtmlCollection toma la lista que le mandemos */
+
+/* OTRA FUNCION  */
+// let integrantes = document.getElementsByTagName("h2");
+// let integranteNum = "";
+// function integrante() {
+//   for (let i = 0; i < integrantes.length; i++) {
+//     integranteNum = integranteNum + integrantes[i].innerText + "\n";
+//   }
+//   return integranteNum;
+// }
+
+// console.log(integrante());
