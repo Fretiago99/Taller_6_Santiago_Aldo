01963b261d74a4894a2a195eec5ecbe3eefff213
Aldo Esteban Vera Zúñiga <aestebanveraz@gmail.com>
2023-11-30 17:17:39 -0300

    bla

--- js/main.js
+++ js/main.js
@@ -1,28 +1,64 @@
+// Acceder al title de html leerlo y mostrarlo por consola
+console.log(document.title);
+
+/* \n */
+
+/* función para crear el nombre completo*/
 function createFullName(htmlCollection) {
+  /* recibe como parametro el listado html q aparece en consola */
+  /* como es una concatenacion vamos instanciar una variable de tipo let */
+
+  /* le asignamos un valor de un string vacio, que va ser mi estado inicial, vamos a ir agregando nombres a este string */
   let fullName = "";
 
+  let counter = 1;
+  let firstName = true;
+
   for (let index = 0; index < htmlCollection.length; index++) {
+    /* index comienza en 0, esto se va a correr siempre y cuando que index sea menor al largo del array, pero en este caso el array 
+    se llama htmlCollection q en este caso va a dar 8, sea va a ir de 0 a 7 y cada vuelta vamos a sumarle 1 al index */
     const element = htmlCollection[index];
+    /* vamos a acceder al elemento de cada vuelta del htmlCollection */
+    if (element.innerText == "") {
+      counter++;
+    }
 
     if (element.tagName === "DD" && element.innerText) {
-      fullName = fullName.concat(`${element.innerText} `);
+      if (firstName) {
+        /* Si viene el primer nombre */
+        fullName = fullName.concat(`${element.innerText} `);
+        firstName = false;
+      } else {
+        if (counter <= 2) {
+          fullName = fullName.concat(`${element.innerText} `);
+        } else {
+          fullName = fullName.concat(`${element.innerText.toUpperCase()} `);
+        }
+      }
+      counter++;
     }
   }
   return `"${fullName.trimEnd()}"`;
 }
 
 const fullList = document.getElementById("divList").children;
+// const listOne = document.getElementById("list1").children;
+/* capturo en una variable el listado 1 de la lista descriptiva con el integrante 1 */
 
+/* Funcion  para mostrar listado*/
 function showList(list) {
+  /* le pasamos por parametro la lista */
   let text = "";
-
+  /* text va a ser el string total q tiene el nro de integrante y nombre de interante */
   for (let i = 0; i < list.length; i++) {
     const elementList = list[i].children;
 
     for (let index = 0; index < elementList.length; index++) {
       const h2dlElem = elementList[index];
+      /* creamos la nueva variable q va a recorrer ahora si los elementos en las diferentes posiciones con el index */
 
       if (h2dlElem.tagName === "H2") {
+        /* si es igual a h2 vamos a concatenar */
         text = text.concat(`${h2dlElem.innerText}: `);
       }
       if (h2dlElem.tagName === "DL") {
@@ -32,7 +68,18 @@ function showList(list) {
   }
   return text;
 }
+
 console.log(`-------\n${showList(fullList)}---------`);
+
+/* Mostrar Formulario */
+function mostrar() {
+  document.getElementById("img").style.display = "block";
+}
+
+/* Ocultar formulario */
+function ocultar() {
+  document.getElementById("img").style.display = "none";
+}
 
 const dl = document.getElementsByTagName("dl");
 
